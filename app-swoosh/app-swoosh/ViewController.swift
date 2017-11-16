923fc313337f1594d851d5f4712111e9ae27926b
SONALI PATEL <patelsonali123@yahoo.com>
2017-11-15 20:05:39 -0500

    Lesson 3 regarding Auto layout and constraints completed successfully

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -16,9 +16,10 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//
+//        bgImg.frame = view.frame
         
-        bgImg.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
