431f91b6718414ae7b06d1cbf6f3e05722265535
SONALI PATEL <patelsonali123@yahoo.com>
2017-12-11 01:32:30 -0500

    Finished with lesson-10 regarding programmatic seagues

--- app-swoosh/app-swoosh/Controller/LeagueViewController.swift
+++ app-swoosh/app-swoosh/Controller/LeagueViewController.swift
@@ -9,30 +9,40 @@
 import UIKit
 
 class LeagueViewController: UIViewController {
-
+    var player: Player!
+    @IBOutlet weak var nextBtn: BorderButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       player = Player()
+    }
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "SkillViewControllerSegue", sender: self)
     }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "Mens")
+//        player.desiredLeague = "Mens"
+//        nextBtn.isEnabled = true
     }
     
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "Womens")
 
-    /*
-    // MARK: - Navigation
+//        player.desiredLeague = "Womens"
+//        nextBtn.isEnabled = true
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
     }
-    */
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "Co-Ed")
 
+//        player.desiredLeague = "Co-Ed"
+//        nextBtn.isEnabled = true
+        
+    }
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
+    }
+   
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Sonali Patel on 12/11/17.
+//  Copyright © 2017 Sonali Patel. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
